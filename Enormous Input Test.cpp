bb88f3385ccf2023dd54e6cc46baf35a48ccb761
sourav singh <40472249+sourav9124@users.noreply.github.com>
2019-10-13 01:05:05 +0530

    Solved Successfully!

---
+++ Enormous Input Test.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,k;
+	cin>>n>>k;
+	int count=0;
+	for(int i=0;i<n;i++)
+	{
+		long long int t;
+		
+		cin>>t;
+		
+		if(t%k==0)
+		{
+			count++;
+			
+		}
+		else
+		{
+			count=count;
+		}
+	}
+	cout<<count<<endl;
+}
