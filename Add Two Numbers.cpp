d98c6910e4d48dd85d951c5365aaf73c8a42fb5a
sourav singh <40472249+sourav9124@users.noreply.github.com>
2019-10-13 01:05:46 +0530

    Solved Successfully!

---
+++ Add Two Numbers.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+      int t;
+      cin>>t;
+      
+      
+      while(t--)
+      {
+      	int a,b;
+        int sum=0;
+        
+          cin>>a;
+          cin>>b;
+      	
+      	sum=a+b;
+      	cout<<sum<<endl;
+	  }
+      
+}
