098e1f711079b21a5d85cd77f77b1a13445df2b3
sourav singh <40472249+sourav9124@users.noreply.github.com>
2019-10-13 13:56:31 +0530

    Solved Successfully!

---
+++ Second Largest.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n=3;
+		
+		
+		int ar[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>ar[i];
+		}
+		sort(ar,ar+n);
+		cout<<ar[1]<<endl;
+		
+		
+		
+		
+	}
+}
