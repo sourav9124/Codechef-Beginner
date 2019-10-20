4fb73ad0ea2f137bdf133557facb28f4101f199c
sourav singh <40472249+sourav9124@users.noreply.github.com>
2019-10-20 10:29:19 +0530

    Solved Successfully !

---
+++ Decrement or Increment.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	if(n%4==0)
+	{
+		n+=1;
+		cout<<n<<endl;
+	}
+	else if(n%4!=0)
+	{
+	 n-=1;
+	 cout<<n<<endl;	
+	}
+}
